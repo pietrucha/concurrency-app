8f68da7ee699de64053abf9ca1516770a2c8ac42
ppietrucha <pawel.pietrucha@qumak.pl>
2017-06-27 16:20:44 +0200

    modify CollectionTest

--- collectionconcurrency/src/test/java/CollectionTest.java
+++ collectionconcurrency/src/test/java/CollectionTest.java
@@ -1,6 +1,7 @@
 import org.junit.Test;
 
 import java.util.concurrent.*;
+import java.util.stream.Stream;
 
 /**
  * Created by ppietrucha on 2017-06-26.
@@ -22,6 +23,63 @@ public class CollectionTest {
         Future<String> submit = executorService.submit(callable);
         submit.cancel(false);
         System.out.println(submit.get());
+    }
+
+    @Test
+    public void testBlockingQueue() throws Exception {
+        BlockingQueue<String> blockingQueue = new LinkedBlockingDeque<>(10);
+
+        ExecutorService es = Executors.newFixedThreadPool(4);
+        Callable<String> cp = () -> {
+            String poll = blockingQueue.poll();
+            return poll;
+        };
+        Runnable co = () -> {
+            try {
+                blockingQueue.put(Thread.currentThread().getName());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        };
+
+        es.submit(co);
+        //Future<String> submit2 = es.submit(cp);
+        Callable<Object> callablewrite = new Callable<Object>() {
+            public Object call() throws Exception {
+                Stream.generate(() -> cp).parallel().map(stringCallable -> {
+                    try {
+                        return es.submit(stringCallable).get();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    } catch (ExecutionException e) {
+                        e.printStackTrace();
+                    }
+                    return null;
+                }).filter(s -> s != null).forEach(System.out::println);
+                return null;
+            }
+        };
+
+        Callable<Object> callable = new Callable<Object>() {
+            public Object call() throws Exception {
+                Stream.generate(() -> co).limit(100_000).parallel().forEach(o -> {
+                    try {
+                        System.out.println("sleep");
+                        TimeUnit.SECONDS.sleep(10);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    es.submit(o);
+                });
+                return null;
+            }
+        };d
+
+        es.submit(callablewrite);
+        es.submit(callable);
+
+        Thread.currentThread().join();
 
     }
 }
